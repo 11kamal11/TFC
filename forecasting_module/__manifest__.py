a34ffb99ce53b3e50a47768a3091228ac7522b70
11kamal11 <kamal9@emails.emizentech.com>
2025-07-01 08:42:44 +0000

    Add 'mail' dependency to support mail.thread inheritance

--- forecasting_module/__manifest__.py
+++ forecasting_module/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'Forecasting Module',
-    'version': '18.0.1.0',
+    'version': '18.0.1.4',
     'summary': 'Future Trend Forecasting Tool for Odoo 18',
     'description': 'A module to upload CSV files and generate time-series forecasts using Prophet.',
     'category': 'Tools',
     'author': 'Your Name',
     'license': 'LGPL-3',
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/forecasting_views.xml',
